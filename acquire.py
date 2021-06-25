8a54ae40760843c34069bef9addbf6f3e56aa45e
Chad Allen <chad.allen.210@gmail.com>
2021-06-25 08:13:14 -0500

    updates

--- acquire.py
+++ acquire.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 
+import matplotlib.pyplot as plt
+
 import os
 from env import host, username, password
 
@@ -60,32 +62,72 @@ def get_zillow_data():
     
     return df
 
+def nulls_by_col(df):
+    '''
+    This function determines how many null values there are in each column, and returns a pandas DataFrame
+    with 3 columns, index=column name, 
+    num_missing=number of missing values per columnm, 
+    prcnt_miss=what percent of each column is null.
+    '''
+    num_missing = df.isnull().sum()
+    rows = df.shape[0]
+    prcnt_miss = num_missing / rows * 100
+    cols_missing = pd.DataFrame({'num_rows_missing': num_missing, 'percent_rows_missing': prcnt_miss})
+    return cols_missing
+
+def nulls_by_row(df):
+    '''
+    This function determines how many null values there are in each row, and returns a pandas DataFrame
+    with 4 columns, index=index, 
+    num_missing=number of missing values per row, 
+    prcnt_miss=what percent of each row is null,
+    num_rows=how many rows are missing this many values.
+    '''
+    num_missing = df.isnull().sum(axis=1)
+    prcnt_miss = num_missing / df.shape[1] * 100
+    rows_missing = pd.DataFrame({'num_cols_missing': num_missing, 'percent_cols_missing': prcnt_miss})\
+    .reset_index()\
+    .groupby(['num_cols_missing', 'percent_cols_missing']).count()\
+    .rename(index=str, columns={'index': 'num_rows'}).reset_index()
+    return rows_missing
+
 def summarize(df):
     '''
-    summarize 
+    This function will take in a single argument (pandas DF)
+    and output to console various statistics on said DF, including:
+    # .head()
+    # .info()
+    # .describe()
+    # value_counts()
+    # observe null values
     '''
-    print('==============================================')
-    print('DataFrame head: ')
+    print('----------------------------------------------------')
+    print('DataFrame Head')
     print(df.head(3))
-    print('==============================================')
-    print('DataFrame info: ')
+    print('----------------------------------------------------')
+    print('DataFrame Info')
     print(df.info())
-    print('==============================================')
-    print('DataFrame description: ')
+    print('----------------------------------------------------')
+    print('DataFrame Description')
     print(df.describe())
-    num_col = [col for col in df.columns if df[col].dtype!='O']
-    cat_col = [col for col in df.columns if col not in num_col]
-    print('==============================================')
-    print('DataFrame value counts: ')
+    num_cols = [col for col in df.columns if df[col].dtype != 'O']
+    cat_cols = [col for col in df.columns if col not in num_cols]
+    print('----------------------------------------------------')
+    print('DataFrame Value Counts: ')
     for col in df.columns:
-        if col in cat_col:
+        if col in cat_cols:
             print(df[col].value_counts())
+            print('--------------------------------------------')
+            print('')
         else:
             print(df[col].value_counts(bins=10, sort=False))
-    print('==============================================')
-    print('nulls in dataframe by column: ')
+            print('--------------------------------------------')
+            print('')
+    print('----------------------------------------------------')
+    print('Nulls in DataFrame by Column: ')
     print(nulls_by_col(df))
-    print('==============================================')
-    print('nulls in dataframe by row: ')
-    print(cols_missing(df))
+    print('----------------------------------------------------')
+    print('Nulls in DataFrame by Rows: ')
+    print(nulls_by_row(df))
+    print('----------------------------------------------------')
     
